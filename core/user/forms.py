e06c1e10a5f22f853b96b6858b570fd9c7042baa
eldiiar <a.eldiyar09@gmail.com>
2022-05-28 17:03:29 +0600

    updated signup form-page for user

--- core/user/forms.py
+++ core/user/forms.py
@@ -4,13 +4,21 @@ from .models import User
 
 
 class SignUpForm(UserCreationForm):
-    username = UsernameField()
-    first_name = forms.CharField()
-    last_name = forms.CharField()
-    email = forms.EmailField()
-    address = forms.CharField()
-    phone_number = forms.CharField()
-
+    username = UsernameField(widget=forms.TextInput(attrs={'class': 'form-control rounded-0', 'placeholder': 'Username'}))
+    first_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control rounded-0',
+                                                               'placeholder': 'Your name (optional)'}))
+    last_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control rounded-0',
+                                                              'placeholder': 'Your lastname (optional)'}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control rounded-0',
+                                                            'placeholder': 'Email'}))
+    address = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control rounded-0',
+                                                            'placeholder': 'Address to delivery'}))
+    phone_number = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control rounded-0',
+                                                                 'placeholder': 'Your phone number'}))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control rounded-0',
+                                                                  'placeholder': 'Password'}))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control rounded-0',
+                                                                  'placeholder': 'Password Again'}))
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'email', 'address', 'phone_number')
