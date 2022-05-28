b78cb2ba3cef9ba680a403c16251558de5f10557
eldiiar <a.eldiyar09@gmail.com>
2022-05-28 16:12:11 +0600

    update user model, added signup

--- core/user/models.py
+++ core/user/models.py
@@ -7,5 +7,5 @@ class User(AbstractUser):
     address = models.CharField(max_length=255, null=True)
     phone_regex = RegexValidator(regex=r'^\+?1?\d{9}$')
     phone_number = models.CharField(validators=[phone_regex], max_length=9, blank=True,)  # validators should be a list
-    photo = models.ImageField(upload_to='user_img', blank=True, default='default/usr_img.png')
+    avatar = models.ImageField(upload_to='user_img', blank=True, default='default/usr_img.png')
     purchases = models.IntegerField(default=0)
