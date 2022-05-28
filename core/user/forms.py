34ade4a24164797a43fa4f091598e375b8215fb2
eldiiar <a.eldiyar09@gmail.com>
2022-05-28 17:38:04 +0600

    updated user model,signup form-page for user, added user-admin

--- core/user/forms.py
+++ core/user/forms.py
@@ -6,19 +6,20 @@ from .models import User
 class SignUpForm(UserCreationForm):
     username = UsernameField(widget=forms.TextInput(attrs={'class': 'form-control rounded-0', 'placeholder': 'Username'}))
     first_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control rounded-0',
-                                                               'placeholder': 'Your name (optional)'}))
+                                                               'placeholder': 'Your name'}))
     last_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control rounded-0',
-                                                              'placeholder': 'Your lastname (optional)'}))
+                                                              'placeholder': 'Your lastname'}))
     email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control rounded-0',
                                                             'placeholder': 'Email'}))
     address = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control rounded-0',
                                                             'placeholder': 'Address to delivery'}))
     phone_number = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control rounded-0',
-                                                                 'placeholder': 'Your phone number'}))
+                                                                 'placeholder': 'Your phone number (507004522)'}))
     password1 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control rounded-0',
                                                                   'placeholder': 'Password'}))
     password2 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control rounded-0',
                                                                   'placeholder': 'Password Again'}))
+
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'email', 'address', 'phone_number')
